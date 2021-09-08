9a03e3281db9d32dd5fe6059f9eceb70789dca2a
Abdulmalek Al-Gahmi <aalgahmi@gmail.com>
2021-09-08 10:55:57 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,16 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    main: './src/javascripts/main.js'
+    main: './src/javascripts/main.js',
+    example01: './src/stylesheets/example01.css',
+    example02: './src/stylesheets/example02.css',
+    example03: './src/stylesheets/example03.css',
+    example04: './src/stylesheets/example04.css',
+    example05: './src/stylesheets/example05.css',
+    example06: './src/stylesheets/example06.scss',
+    example07: './src/stylesheets/example07.scss',
+    example08: './src/stylesheets/example08.scss',
+    example09: './src/stylesheets/example09.scss'
   },
   mode: 'development',
   devtool: 'inline-source-map',
